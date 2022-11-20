fad96510331a1fa557b86a081a15eb3037b27186
ttoghere <tuncsoftwarehere@gmail.com>
2022-11-20 16:19:34 +0300

    Will BuildContext methods

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
+import 'package:blocoverview/cubits/counter_cubit/counter_cubit.dart';
 import 'package:blocoverview/screens/homepage.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Material App',
-      home: Homepage(),
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Material App',
+        home: Homepage(),
+      ),
     );
   }
 }
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,4 +1,7 @@
+import 'package:blocoverview/cubits/counter_cubit/counter_cubit.dart';
+import 'package:blocoverview/screens/other_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({
@@ -7,18 +10,43 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var blocProvider = BlocProvider.of<CounterCubit>(context, listen: true);
     return Scaffold(
-      body: Center(
-        child: Text(
-          '0',
-          style: Theme.of(context).textTheme.headline4,
-        ),
+      //BlocListener kapsamı olana BlocConsumer
+      //Provider Consumer Widget'ı ile Bloc için aynı işlevi sağlar
+      body: BlocConsumer<CounterCubit, CounterState>(
+        listener: (context, state) {
+          if (state.counter == 3) {
+            showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                      content: Text("Counter is ${state.counter}"),
+                    ));
+          } else if (state.counter == -1) {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => OtherPage()));
+          }
+        },
+        builder: (context, state) {
+          return BlocBuilder<CounterCubit, CounterState>(
+            builder: (context, state) {
+              return Center(
+                child: Text(
+                  '${state.counter}',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              );
+            },
+          );
+        },
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
-            onPressed: () {},
+            onPressed: () {
+              blocProvider.increment();
+            },
             child: Icon(Icons.add),
             heroTag: "Increment",
           ),
@@ -26,7 +54,9 @@ class Homepage extends StatelessWidget {
             width: 10,
           ),
           FloatingActionButton(
-            onPressed: () {},
+            onPressed: () {
+              blocProvider.decrement();
+            },
             child: Icon(Icons.remove),
             heroTag: "Decrement",
           )
@@ -35,3 +65,66 @@ class Homepage extends StatelessWidget {
     );
   }
 }
+
+
+// class Homepage extends StatelessWidget {
+//   const Homepage({
+//     Key? key,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var blocProvider = BlocProvider.of<CounterCubit>(context, listen: true);
+//     return Scaffold(
+//       //BlocBuilder return üstü provider tanımlamak ile eş değerdir
+//       //Ama aktif Değişimleri kaldırması için BlocListener ile kaplanması gereklidir
+//       //Fonksiyonları listener yapısı içine taşırız
+//       body: BlocListener<CounterCubit, CounterState>(
+//         listener: (context, state) {
+//           if (state.counter == 3) {
+//             showDialog(
+//                 context: context,
+//                 builder: (context) => AlertDialog(
+//                       content: Text("Counter is ${state.counter}"),
+//                     ));
+//           } else if (state.counter == -1) {
+//             Navigator.of(context)
+//                 .push(MaterialPageRoute(builder: (context) => OtherPage()));
+//           }
+//         },
+//         child: BlocBuilder<CounterCubit, CounterState>(
+//           builder: (context, state) {
+//             return Center(
+//               child: Text(
+//                 '${state.counter}',
+//                 style: Theme.of(context).textTheme.headline4,
+//               ),
+//             );
+//           },
+//         ),
+//       ),
+//       floatingActionButton: Row(
+//         mainAxisAlignment: MainAxisAlignment.end,
+//         children: [
+//           FloatingActionButton(
+//             onPressed: () {
+//               blocProvider.increment();
+//             },
+//             child: Icon(Icons.add),
+//             heroTag: "Increment",
+//           ),
+//           SizedBox(
+//             width: 10,
+//           ),
+//           FloatingActionButton(
+//             onPressed: () {
+//               blocProvider.decrement();
+//             },
+//             child: Icon(Icons.remove),
+//             heroTag: "Decrement",
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
